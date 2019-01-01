c96f9e520eddb499da6bba05c654117f9f49b485
Brahim Bahaida <bbahieda@gmail.com>
2019-01-01 17:58:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# spring_ms_jwt_mongo_angular" 
---
+++ categorie-service/src/main/java/com/bbahaida/CategorieServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bbahaida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorieServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategorieServiceApplication.class, args);
+    }
+
+}
+
